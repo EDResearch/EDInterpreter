79036ae53d9c9294055397c9aee7210175b1feeb
iwin2471 <iwin12372@gmail.com>
2017-03-23 12:29:38 +0900

    [syntax] test if

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-
-var data = fs.readFileSync(process.argv[2], 'utf8');
+var async = require('async');
+var data;
 var tok = "";
+var state = 0;
+var string = "";
 
-for(var i in data){
-  tok+=data[i];
-  if(tok == " "){
-    tok = ""
-  }else if(tok === "log"){
-    console.log("found a log");
+async.waterfall([
+  (callback)=>{
+    fs.readFile(process.argv[2], 'utf8',(err, Rdata)=>{
+      if(err){
+        console.log("cannot open"+process.argv[2]);
+        callback("err", "a");
+      }
+      callback(null, Rdata);
+    });
+  },
+
+  (data, callback)=>{
+    for(var i in data){
+      tok+=data[i];
+      if(tok == " "){
+        if(!state)
+          tok = "";
+        else
+          tok = " ";
+      }else if(tok === "log"){
+        tok = "";
+      }else if(tok === "\n"){
+        tok = "";
+      }else if(tok === "\""){
+        if(!state){
+          state = 1;
+        }else{
+          console.log(string);
+          state = 0;
+          string = "";
+          tok = "";
+        }
+      }else if(state){
+        if(data[i] !== "\""){
+          string += data[i];
+          tok = "";
+        }else{
+          console.log(string);
+          string = "";
+          tok == "";
+          state = 0;
+        }
+      }else if(tok === "if"){
+         tok == "";
+      }
+    }
   }
-}
+], (err, result)=>{
+   if(err) return 0;
+   else data = result;
+});
+
+
--- test.js
+++ test.js
@@ -1,1 +1,5 @@
 log "hello world!"
+log "hello world!"
+if 1 == 1{
+  log "hell"
+}
